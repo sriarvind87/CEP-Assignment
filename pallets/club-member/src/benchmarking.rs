c1b173d21231690b4f15c8fee0bd7239f76c1205
aideepakchaudhary <aideepakchaudhary@gmail.com>
2022-04-29 20:42:39 +0530

    refactor code

--- pallets/club-member/src/benchmarking.rs
+++ pallets/club-member/src/benchmarking.rs
@@ -1,19 +1,19 @@
 //! Benchmarking setup for pallet-club-member
 
 use super::{Pallet as ClubMember, *};
-use frame_benchmarking::{account, benchmarks_instance_pallet, whitelist};
-use frame_support::{assert_ok, traits::EnsureOrigin};
-use frame_system::RawOrigin;
+use frame_benchmarking::{account, benchmarks, benchmarks_instance_pallet, whitelist};
+use frame_benchmarking::BenchmarkParameter::m;
+use frame_support::{assert_noop, assert_ok, traits::EnsureOrigin};
+use frame_system::{Origin, RawOrigin};
 
 const SEED: u32 = 0;
 
-benchmarks_instance_pallet! {
+benchmarks! {
 		add_member {
-			let members = (0..m).map(|i| account("member", i, SEED)).collect::<Vec<T::AccountId>>();
-			set_members::<T>(members.clone(), None);
-			let new_member = account::<T::AccountId>("add", m, SEED);
+			let members = (0..20).map(|i| account("member", i, SEED)).collect::<Vec<T::AccountId>>();
+			let new_member = account::<T::AccountId>("add", 20, SEED);
 		}: {
-			assert_ok!(ClubMember::<T>::add_member(T::AddOrigin::successful_origin(), new_member.clone()));
+			assert_ok!(ClubMember::<T>::add_member(RawOrigin::Root, new_member.clone()));
 		}
 		verify {
 			assert!(ClubMembers::<T>::get().contains(&new_member));
@@ -22,13 +22,14 @@ benchmarks_instance_pallet! {
 
 
 		remove_member {
-			let members = (0..m).map(|i| account("member", i, SEED)).collect::<Vec<T::AccountId>>();
-			set_members::<T>(members.clone(), Some(members.len() - 1));
+
+			let members = (0..20).map(|i| account("member", i, SEED)).collect::<Vec<T::AccountId>>();
 			let to_remove = members.first().cloned().unwrap();
+
 		}: {
-			assert_ok!(ClubMembers<T>::remove_member(T::RemoveOrigin::successful_origin(), to_remove.clone()));
+			assert_ok!(ClubMembers::<T>::remove_member(RawOrigin::Root, to_remove.clone()));
 		} verify {
-			assert!(!ClubMembers<T>::get().contains(&to_remove));
+			assert_noop!(ClubMembers::<T>::get());
 			#[cfg(test)] crate::tests::clean();
 		}
 	}
