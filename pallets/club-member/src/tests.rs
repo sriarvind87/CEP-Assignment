fdb1cab85ed7d3d56c9c358280973dd1aecc2a9a
aideepakchaudhary <aideepakchaudhary@gmail.com>
2022-04-29 20:14:01 +0530

    refactor code

--- pallets/club-member/src/tests.rs
+++ pallets/club-member/src/tests.rs
@@ -22,7 +22,7 @@ fn member_already_added_to_club() {
 #[test]
 fn remove_member_from_club() {
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
+
 		assert_ok!(ClubMember::add_member(Origin::root(), 1));
 		assert_ok!(ClubMember::remove_member(Origin::root(), 1));
 	});
@@ -31,10 +31,10 @@ fn remove_member_from_club() {
 #[test]
 fn not_club_member() {
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
+
 		assert_ok!(ClubMember::add_member(Origin::root(), 1));
 		assert_ok!(ClubMember::remove_member(Origin::root(), 1));
-		// Ensure the expected error is thrown when member is already added.
+		// Ensure the expected error is thrown when member not a member.
 		assert_noop!(ClubMember::remove_member(Origin::root(), 1), Error::<Test>::NotMember);
 	});
 }
